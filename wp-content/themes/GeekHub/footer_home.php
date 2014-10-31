e91999260d29a35145efb6b984ce788afa146224
leybal <leybal@ukr.net>
2014-10-31 19:49:05 +0200

    Index + Another page

---
+++ wp-content/themes/GeekHub/footer_home.php
@@ -0,0 +1,15 @@
+<ul id="footer">
+    <li>
+        <ul class="nav">
+            <li><a href="/wordpress/">BLABLALBA</a></li>
+            <li><a href="/about.html">Про GeekHub</a></li>
+            <li><a href="/team.html">Команда</a></li>
+            <li><a href="/contacts.html">Зв'язок</a></li>
+        </ul>
+    </li>
+    <li>© Copyright 2011</li>
+</ul>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
--- wp-content/themes/GeekHub/index.php
+++ wp-content/themes/GeekHub/index.php
@@ -4,7 +4,15 @@ Template Name: Archives with Content
 */
 ?>
 
-<?php get_header(); ?>
+<?php
+if ( is_home() ) :
+    get_header( 'home' );
+elseif ( is_404() ) :
+    get_header( '404' );
+else :
+    get_header();
+endif;
+?>
 
 
  <?php if (have_posts()) : while (have_posts()) : the_post();?>
@@ -20,4 +28,12 @@ Template Name: Archives with Content
 
 
 
-<?php get_footer(); ?>
+<?php
+    if ( is_home() ) :
+        get_footer( 'home' );
+    elseif ( is_404() ) :
+        get_footer( '404' );
+    else :
+        get_footer();
+endif;
+?>
